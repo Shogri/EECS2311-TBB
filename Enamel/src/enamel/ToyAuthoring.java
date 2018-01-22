9949e19f9dd6d6ec638884f3580155d57e0e0f25
amaanvania <amaanv@hotmail.com>
2018-01-21 22:41:34 -0500

    Lab 2 push

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,9 +1,24 @@
 package enamel;
 
-public class ToyAuthoring {
+import java.awt.Component;
+import java.io.IOException;
 
-    public static void main(String[] args) { 	  	
-    	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
-    }
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class ToyAuthoring {
+	private static Component parent;
+
+	public static void main(String[] args) throws IOException {
+
+		ScenarioParser s = new ScenarioParser(true);
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Factory Scenario Files", "txt");
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(parent);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+			s.setScenarioFile(chooser.getSelectedFile().getAbsolutePath()); //uses JFileChooser to generate path of file
+		}
+	}
 }
