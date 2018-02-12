340cc3ff63d22f9984eb60767b956544199df1fa
Shogri <aalshoghri@gmail.com>
2018-02-12 18:46:25 -0500

    Added more methods to lineeditor

--- Enamel/src/enamel/LineEditor.java
+++ Enamel/src/enamel/LineEditor.java
@@ -94,4 +94,61 @@ public class LineEditor { //model in the model view controller for the scenario 
 		fw.write("/~sound:" + soundFileName);
 		fw.close();
 	}
+	
+	public static void addSkip(File file, String identifier) throws IOException
+	{
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+		fw.write(System.lineSeparator());
+		fw.write("/~skip:" + identifier);
+		fw.close();
+	}
+	
+	public static void addDispClearAll(File file) throws IOException
+	{
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+		fw.write(System.lineSeparator());
+		fw.write("/~disp-clearAll");
+		fw.close();
+	}
+	
+	public static void addDispClearCell(File file, int cellnum) throws IOException
+	{
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+		fw.write(System.lineSeparator());
+		fw.write("/~disp-clear-cell:" + cellnum);
+		fw.close();
+	}
+	
+	public static void addDispCellPins(File file, int cellnum, int sequence) throws IOException
+	{
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+		fw.write(System.lineSeparator());
+		fw.write("/~disp-cell-pins:" + cellnum + " " + sequence);
+		fw.close();
+	}
+	
+	public static void addDispCellChar(File file, int cellnum, String character) throws IOException
+	{
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+		fw.write(System.lineSeparator());
+		fw.write("/~disp-cell-char:" + cellnum + " " + character);
+		fw.close();
+	}
+	
+	public static void addDispCellRaise(File file, int cellnum, int pinnum) throws IOException
+	{
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+		fw.write(System.lineSeparator());
+		fw.write("/~disp-cell-raise:" + cellnum + " " + pinnum);
+		fw.close();
+	}
+	
+	public static void addDispCellLower(File file, int cellnum, int pinnum) throws IOException
+	{
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+		fw.write(System.lineSeparator());
+		fw.write("/~disp-cell-lower:" + cellnum + " " + pinnum);
+		fw.close();
+	}
+	 
 }
